4a7d003d2625f95b67e050351432577bbc791f5f
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2023-03-12 21:44:54 +0900

    マイグレーションファイル修正

--- src/back/models/models.py
+++ src/back/models/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Integer, Column, String, DateTime, ForeignKey, text
-from sqlalchemy.dialects.mysql import TEXT
+from sqlalchemy.dialects.mysql import MEDIUMTEXT
 from sqlalchemy.orm import relationship
 
 from database import Base
@@ -9,12 +9,12 @@ from datetime import datetime
 class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True)
-    user_id = Column(String(200), nullable=False, unique=True)
-    username = Column(String(200), nullable=False)
-    name = Column(String(200), nullable=False)
-    profile_image_url = Column(String(1000), nullable=False)
-    session_id = Column(String(200), nullable=False)
-    refresh_token = Column(String(1000), nullable=False)
+    user_id = Column(String(255), nullable=False, unique=True)
+    username = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=False)
+    profile_image_url = Column(String(255), nullable=False)
+    session_id = Column(String(255), nullable=False)
+    refresh_token = Column(String(255), nullable=False)
     created_at = Column(DateTime, nullable=False, server_default=text('CURRENT_TIMESTAMP'))
     updated_at = Column(DateTime, default=datetime.now, server_default=text('CURRENT_TIMESTAMP'), server_onupdate=text('CURRENT_TIMESTAMP'))
 
@@ -25,17 +25,17 @@ class User(Base):
 class Category(Base):
     __tablename__ = "category"
     id = Column(Integer, primary_key=True)
-    name = Column(String(200), nullable=False, unique=True)
-    img_url = Column(TEXT, nullable=False)
+    name = Column(String(255), nullable=False, unique=True)
+    img_url = Column(String(255), nullable=False)
     users = relationship('TweetUser', backref='category')
 	
 
 class TweetUser(Base):
     __tablename__ = "tweet_user"
     id = Column(Integer, primary_key=True)
-    user_id = Column(String(200), nullable=False, unique=True)
-    url =  Column(TEXT, nullable=False)
-    profile_img_url = Column(TEXT, nullable=False)
+    user_id = Column(String(255), nullable=False, unique=True)
+    url =  Column(String(255), nullable=False)
+    profile_img_url = Column(String(255), nullable=False)
     category_id = Column(Integer, ForeignKey('category.id'), nullable=False)
     tweets = relationship('Tweet', backref='tweet_user')
 
@@ -43,9 +43,9 @@ class TweetUser(Base):
 class Tweet(Base):
     __tablename__ = "tweet"
     id = Column(Integer, primary_key=True)
-    tweet_id = Column(String(200), nullable=False, unique=True)
-    tweet_en = Column(String(1000), nullable=False)
-    tweet_ja = Column(String(1000), nullable=False)
+    tweet_id = Column(String(255), nullable=False, unique=True)
+    tweet_en = Column(MEDIUMTEXT, nullable=False)
+    tweet_ja = Column(MEDIUMTEXT, nullable=False)
     category_id = Column(Integer, ForeignKey('category.id'), nullable=False)
     tweet_user_id = Column(Integer, ForeignKey('tweet_user.id'), nullable=False)
     tweeted_at = Column(DateTime, default=datetime.now, nullable=False)
@@ -68,8 +68,7 @@ class SecondDefaultCategory(Base):
 class Book(Base):
   __tablename__ = "book"
   id = Column(Integer, primary_key=True)
-  img = Column(String(1000), nullable=False)
-  title = Column(String(1000), nullable=False)
+  img = Column(String(255), nullable=False)
+  title = Column(String(255), nullable=False)
   description = Column(String(1000), nullable=False)
-  url = Column(String(1000), nullable=False)
-  category_id = Column(Integer, ForeignKey('category.id'))
+  url = Column(String(255), nullable=False)
